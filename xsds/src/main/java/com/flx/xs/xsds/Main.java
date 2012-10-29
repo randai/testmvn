25239e47ee891fd60d10cc7d841b7378aad40390
Andy Crutchlow <andycrutchlow@gmail.com>
2012-10-29 17:33:09 -0400

    refine logging

--- xsds/src/main/java/com/flx/xs/xsds/Main.java
+++ xsds/src/main/java/com/flx/xs/xsds/Main.java
@@ -1,19 +1,33 @@
 package com.flx.xs.xsds;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.flx.xs.common.App;
+import com.flx.xs.common.logger.Logger;
+import com.flx.xs.xsds.IRunner;
+import com.flx.xs.xsds.Main;
+
 /**
- * Created by IntelliJ IDEA.
- * User: xaoc
- * Date: 30.11.11
- * Time: 17:20
+ * Created by andy.
+
  */
 public class Main {
+    @Logger private static Log log;
+
     public static void main(String[] args)
     {
-
-        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
-				"classpath:/META-INF/application-context-root.xml");
+    	String pathToApplicationContext = System.getProperty("pathToApplicationContext","classpath:/application-context.xml");
+    	String[] contextPaths;
+    	if(args.length > 0) {
+    		contextPaths = new String[] {pathToApplicationContext,args[0]};
+    	} else {
+    		contextPaths = new String[] {pathToApplicationContext};
+    	}
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(contextPaths);
+        log = LogFactory.getLog(Main.class);
+        log.info(App.CommonMethod());
         IRunner runner = ctx.getBean("runner", IRunner.class);
         runner.run();
     }
