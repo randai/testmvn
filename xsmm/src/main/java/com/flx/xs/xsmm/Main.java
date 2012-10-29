106c4204a866224cb6e4a9e47363d431f979fc21
Andy Crutchlow <andycrutchlow@gmail.com>
2012-10-29 17:18:16 -0400

    Adding logback for logging

--- xsmm/src/main/java/com/flx/xs/xsmm/Main.java
+++ xsmm/src/main/java/com/flx/xs/xsmm/Main.java
@@ -1,13 +1,19 @@
 package com.flx.xs.xsmm;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.flx.xs.common.App;
+import com.flx.xs.common.logger.Logger;
 /**
  * Created by andy.
  * Main class to start an application
  */
 public class Main {
+	
+    @Logger private static Log log;
+
     public static void main(String[] args)
     {
     	String pathToApplicationContext = System.getProperty("pathToApplicationContext","classpath:/application-context.xml");
@@ -18,7 +24,8 @@ public class Main {
     		contextPaths = new String[] {pathToApplicationContext};
     	}
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(contextPaths);
-
+        log = LogFactory.getLog(Main.class);
+        log.info(App.CommonMethod());
         IRunner runner = ctx.getBean("runner", IRunner.class);
         runner.run();
     }
