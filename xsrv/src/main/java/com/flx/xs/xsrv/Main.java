0a435c56d87dddbc26c6ff86d7c7935199165255
Andy Crutchlow <andycrutchlow@gmail.com>
2012-10-29 17:46:05 -0400

    logging

--- xsrv/src/main/java/com/flx/xs/xsrv/Main.java
+++ xsrv/src/main/java/com/flx/xs/xsrv/Main.java
@@ -1,6 +1,13 @@
 package com.flx.xs.xsrv;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.flx.xs.common.App;
+import com.flx.xs.common.logger.Logger;
+import com.flx.xs.xsrv.IRunner;
+import com.flx.xs.xsrv.Main;
 
 /**
  * Created by Andy.
@@ -9,16 +16,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * The ENV specific settings are propagated in the the master.properties and 
  */
 public class Main {
+    @Logger private static Log log;
+
     public static void main(String[] args)
     {
+    	String pathToApplicationContext = System.getProperty("pathToApplicationContext","classpath:/application-context.xml");
     	String[] contextPaths;
     	if(args.length > 0) {
-    		contextPaths = new String[] {"classpath:/META-INF/application-context-root.xml",args[0]};
+    		contextPaths = new String[] {pathToApplicationContext,args[0]};
     	} else {
-    		contextPaths = new String[] {"classpath:/META-INF/application-context-root.xml"};
+    		contextPaths = new String[] {pathToApplicationContext};
     	}
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(contextPaths);
-
+        log = LogFactory.getLog(Main.class);
+        log.info(App.CommonMethod());
         IRunner runner = ctx.getBean("runner", IRunner.class);
         runner.run();
     }
