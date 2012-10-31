f22d8d7c9e4f1d83ea56b77c2c6c2f5719b543b7
Andy Crutchlow <andyc@cogeco.ca>
2012-10-30 22:58:49 -0400

    more refinements

--- xsrv/src/main/java/com/flx/xs/xsrv/demo/DemoTakerApi.java
+++ xsrv/src/main/java/com/flx/xs/xsrv/demo/DemoTakerApi.java
@@ -1,5 +1,11 @@
 package com.flx.xs.xsrv.demo;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 import org.apache.commons.logging.Log;
 import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.jmx.export.annotation.ManagedResource;
@@ -10,11 +16,36 @@ import com.flx.xs.xsrv.TakerApi;
 //@ManagedResource(objectName="xsrv:name=DemoTaker", description="My Demo Taker")
 public class DemoTakerApi implements TakerApi  {
 	@Logger private Log log;
+	
+	String fileName;
+	
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
 	@Override
 	public void initialize() {
-		log.info("In initialize");
-		
+		log.info("In initialize fileName="+fileName);
+		try{
+			FileInputStream fstream = new FileInputStream(fileName);
+		  // Get the object of DataInputStream
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+		  //Read File Line By Line
+			while ((strLine = br.readLine()) != null)   {
+		  // Print the content on the console
+				System.out.println (strLine);
+			}
+		  //Close the input stream
+			in.close();
+		}catch (Exception e){//Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		}
 	}
+
 	//@ManagedOperation(description="dispose")
 	@Override
 	public void dispose() {
